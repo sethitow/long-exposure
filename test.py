5b6321b34153d1ca57ef55bebb8b1618a8f9facf
Seth Itow <seth@Seths-MacBook-Pro.local>
2018-10-10 00:05:12 -0700

    Greatest value along timeseries algorithm

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ import numpy as np
 import copy
 
 print('Opening video file')
-# videodata = skv.FFmpegReader('spxl.mov')
-videodata = skv.FFmpegReader(skvideo.datasets.bigbuckbunny())
-shape = videodata.getShape()
+videodata = skv.FFmpegReader('spxl.mov')
+# videodata = skv.vread(skvideo.datasets.bigbuckbunny())
+shape = videodata.shape
 num_of_frames = shape[0]
 img_shape = shape[1:]
 
@@ -16,10 +16,22 @@ print(f"Number of Frames: {num_of_frames}")
 
 stack = np.zeros(img_shape)
 
-for frame in videodata:
-	stack = stack + frame
-				
-stack = stack / num_of_frames / 255
+def rms(x):
+	return np.sqrt(np.mean(np.square(x)))
+vrms = np.vectorize(rms)
+
+iterations = img_shape[0] * img_shape[1]
+counter = 0
+for x in range(0, img_shape[0]):
+	for y in range(0, img_shape[1]):
+		counter = counter + 1
+		print(counter/iterations)
+		tuples = videodata[:,x,y,:]
+		t_rms = np.apply_along_axis(rms, 1, tuples) 
+		value = tuples[np.argmax(t_rms)]	
+		stack[x,y] = value
+						
+stack = stack / 255
 
 ski.imsave('test.png', stack)
 
