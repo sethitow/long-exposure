5ba86c9cad948540585dd0ec5d8e0d6d7bb28235
Seth Itow <sethitow@example.com>
2019-06-03 20:37:24 -0700

    Try numba

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import skvideo.datasets
 import skimage.io as ski
 import numpy as np
 import copy
+import numba
+from numba import jit
 
 print('Opening video file')
 videodata = skv.vread('spxl.mov')
@@ -14,23 +16,29 @@ img_shape = shape[1:]
 print(f"Image size: {img_shape}")
 print(f"Number of Frames: {num_of_frames}")
 
-stack = np.zeros(img_shape)
 
 def rms(x):
 	return np.sqrt(np.mean(np.square(x)))
-vrms = np.vectorize(rms)
 
-iterations = img_shape[0] * img_shape[1]
-counter = 0
-for x in range(0, img_shape[0]):
-	for y in range(0, img_shape[1]):
-		counter = counter + 1
-		print(counter/iterations)
-		tuples = videodata[:,x,y,:]
-		t_rms = np.apply_along_axis(rms, 1, tuples) 
-		value = tuples[np.argmax(t_rms)]	
-		stack[x,y] = value
-						
+@jit(nopython=True)
+def get_value(tuples):
+	means = []
+	for item in tuples:
+		means.append(np.mean(item))
+	return tuples[np.argmax(means)]
+
+def run(img_shape, videodata):
+	stack = np.zeros(img_shape)
+	iterations = img_shape[0] * img_shape[1]
+	counter = 0
+	for x in range(0, img_shape[0]):
+		for y in range(0, img_shape[1]):
+			counter = counter + 1
+			print(counter/iterations)
+			print(numba.typeof(videodata[:,x,y,:]))
+			print("gothere")
+			stack[x,y] = get_value(videodata[:,x,y,:])
+stack = run(img_shape, videodata)	
 stack = stack / 255
 
 ski.imsave('test.png', stack)
